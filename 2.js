4af4106abc772fa6b852ee3e92cbe10ac69c6ea6
ChoiceHotelsRblx <44909121+ChoiceHotelsRblx@users.noreply.github.com>
2018-11-09 21:10:16 +0000

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,111 @@
+const Discord = require ("discord.js");
+const bot = new Discord.Client();
+
+bot.on("message", function (message){
+    if (message.content == "hello")
+  ( 
+    message.reply("Hello!")
+  )
+
+  if (message.content == "djjames")
+  ( 
+    message.reply("https://www.twitch.tv/jamesalexander098")
+  )
+
+  if (message.content == "djreal")
+  ( 
+    message.reply("https://www.twitch.tv/realksi99")
+  )
+  if (message.content == "bye")
+  ( 
+    message.reply("Bye!")
+  )
+
+  if (message.content == "help")
+  ( 
+    message.reply("djreal,djjames,who am i,bye,Im leaving,group and ranks are the commands.")
+  )
+
+  if (message.content == ("group")
+  
+   ) message.reply("https://www.roblox.com/My/Groups.aspx?gid=3066684")
+  
+
+
+  
+  if (message.content == "ranks")
+  ( 
+    message.reply("19 | Chairman,18 | Vice Chairman,17 | President,16 | Vice President, 15 | Board Of Directors,14 | Public Relation Ship's Officer,13 | Builder/GFX,12 | Senior Management,11 |  Management,10 |  Head of Receptionist,9 |  Head of Security,8 | Receptionist,7 |  Security,6 |  Bar,5 |  House keeping,4 | Honored Member,2 | Awaiting training,1 | Hotel Guest are the ranks.")
+  )
+
+
+  if (message.content == "who am i")
+  ( 
+    message.reply("a discord user")
+  )
+
+
+  if (message.content == "who am i")
+  ( 
+    message.reply("a discord user")
+  )
+
+console.log('Bot loaded. Have fun!')
+
+console.log("No,Mayonase is not an instrument")
+
+if (message.content == "Who made you?")
+  ( 
+    message.reply("CynosasFate")
+  )
+
+  if (message.content == "Demoted")
+  ( 
+    message.reply("rekt amirite")
+  )
+    
+if (message.content == "far away")
+  ( 
+    message.reply("where")
+  )
+  
+if (message.content == "okok")
+  ( 
+    message.reply("meow")
+  )
+
+
+if (message.content == "Free money!")
+  ( 
+    message.reply("How much?")
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;bot.on ("ready", function() {}
+
+,
+console.log("Ready!"));
+
+
+
+
+
+
+
+
+
+});
+
+bot.login ("NTA0NzAyMjc4NTk5NTczNTA0.DrNPlg.CCLxYwL2-_15aaygzETo1CRX_8o");
